9ae1101ee03cc9ceeca54dadfa17e089ab392150
Ramon-Rabello <109632201+Ramon-Rabello@users.noreply.github.com>
2025-11-30 16:59:52 -0300

    fix/correção-teste-sistema-pessoa-entrar

---
+++ src/test/java/frontend/PessoaEntrarTest.java
@@ -0,0 +1,30 @@
+package frontend;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
+
+public class PessoaEntrarTest extends AssertJSwingJUnitTestCase {
+
+    private FrameFixture window;
+
+    @Override
+    protected void onSetUp() {
+        EntrarPessoa frame = GuiActionRunner.execute(() -> new EntrarPessoa());
+        
+        window = new FrameFixture(robot(), frame);
+        window.show(); 
+    }
+
+    @Test
+    public void deveExibirErroComCredenciaisInvalidas() {
+        window.textBox("loginEmail").enterText("Testedefalha@teste.com");
+
+        window.textBox("loginSenha").enterText("senhaerrada");
+
+        window.button("botaoEntrar").click();
+
+        window.optionPane().requireInformationMessage().requireMessage("Erro, email ou senha incorretos!");
+    }
+}
